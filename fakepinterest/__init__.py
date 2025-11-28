b9d048da5a34e3cdfd76b47a10ad91534b0c6aa7
Eduardo Azevedo <eduardo-francisco@hotmail.com>
2025-11-28 15:57:05 -0300

    nova base de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
 app.config["SECRET_KEY"] = "8ede992bc6fd1dc0058bf93c35093d03"
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
