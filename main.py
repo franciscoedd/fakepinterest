1c9d9a1cf64444cb41d7e5fe421396d080a3b78f
Eduardo Azevedo <eduardo-francisco@hotmail.com>
2025-11-28 17:42:02 -0300

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-
 from fakepinterest import app
+import os
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
