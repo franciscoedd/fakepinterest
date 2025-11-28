0c3cba941c39805db0cc85f9bea92366712b3e8c
Eduardo Azevedo <eduardo-francisco@hotmail.com>
2025-11-28 16:35:25 -0300

    atualização porta

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+
 from fakepinterest import app
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
